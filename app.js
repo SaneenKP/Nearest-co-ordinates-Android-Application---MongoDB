d3678577f67c826d2dac0fc36687fe32ca45098a
saneenKP <saneenkp2000@gmail.com>
2022-04-02 22:18:06 +0530

    url created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const {MongoClient} = require('mongodb');
 
 async function main(){
-    
+    const url = 'mongodb+srv://saneen:<$@neenKP8120>@cluster0.xjspj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 }
