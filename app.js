c8c246e93313a0b9dfde70ca2b323e3ae7d1d0d1
saneenKP <saneenkp2000@gmail.com>
2022-04-05 00:09:58 +0530

    client connection wrapped in try catch

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const { MongoClient } = require('MongoClient')
 var main = async () => {
 
     const uri = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mny1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    const client  = new MongoClient(uri);
+
+    await client.connect();
 
 }
