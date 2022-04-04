e0bfeb607134e8e889f07fe5ce2a15f43791459f
saneenKP <saneenkp2000@gmail.com>
2022-04-04 23:43:33 +0530

    removed all code

--- app.js
+++
@@ -1,6 +0,0 @@
-const {MongoClient} = require('mongodb');
-
-async function main(){
-    const url = 'mongodb+srv://saneen:<$@neenKP8120>@cluster0.xjspj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-}
