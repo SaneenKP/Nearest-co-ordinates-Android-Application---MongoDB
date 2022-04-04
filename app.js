f6beff9d2b0f054be2fa0db09574aca0d63437f9
saneenKP <saneenkp2000@gmail.com>
2022-04-05 00:10:43 +0530

    client connection closed in finally statement

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var main = async () => {
     const uri = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mny1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     const client  = new MongoClient(uri);
 
-    await client.connect();
+    try{
+        await client.connect();
+    }catch(e){
+        console.log("error :" + e);
+    }finally{
+        await client.close();
+    }
 
 }
