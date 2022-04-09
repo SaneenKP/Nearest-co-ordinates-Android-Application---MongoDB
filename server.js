df002e2d2fbe89b2ebc71fd828033e5212d37405
saneenKP <saneenkp2000@gmail.com>
2022-04-09 23:42:12 +0530

    url parsing done

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const url = require('url');
+
+const PORT = (process.env.PORT) ? process.env.PORT : 8080;
+
+const server = http.createServer((req , res) => {
+    var parsedURL = url.parse(req.url , true);
+    var path = parsedURL.pathname;
+})
+
+server.listen(PORT , () => {
+    console.log("server listening at "+PORT);
+})
