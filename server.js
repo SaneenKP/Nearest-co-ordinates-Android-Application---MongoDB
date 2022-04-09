4848946aaa2679dbc8a9653328489c768fc475a5
saneenKP <saneenkp2000@gmail.com>
2022-04-09 23:43:56 +0530

    query string added

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const PORT = (process.env.PORT) ? process.env.PORT : 8080;
 const server = http.createServer((req , res) => {
     var parsedURL = url.parse(req.url , true);
     var path = parsedURL.pathname;
+    var queryString = parsedURL.query;
+
+    
+
+
+
 })
 
 server.listen(PORT , () => {
