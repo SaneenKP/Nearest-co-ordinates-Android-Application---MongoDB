082625ef68c35580bd792d7b1df706682e071138
saneenKP <saneenkp2000@gmail.com>
2022-04-10 15:39:53 +0530

    mongo db conection working but late

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const { type } = require('os');
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
 
+const {MongoClient} = require('mongodb');
+const { config } = require('process');
+
 const PORT = (process.env.PORT) ? process.env.PORT : 3000;
 
 const server = http.createServer((req , res) => {
+
     var parsedURL = url.parse(req.url , true);
     var path = parsedURL.pathname;
     var queryString = parsedURL.query;
@@ -44,17 +48,34 @@ var routes = {
     nearByLocation : (data , res) => {
 
         var {coordinates} = JSON.parse(data)
-        console.log(coordinates[0]);
+        connectMongoDB().catch(console.error)
         res.setHeader('Content_Type','application/json')
         res.setHeader('Access-Control-Allow-Origin','*')
-        res.writeHead(200)
+        res.write(JSON.stringify(coordinates))
         res.end()
     },
     notFound : (data , res) => {
         res.write("No Result Found");
         res.end()
     },
-    saneen : () => {
-        console.log("this is saneen");
+    saneen : (data,res) => {
+        res.write("Thi is saneen")
+        res.end()
+    }
+}
+
+var connectMongoDB = async () => {
+
+    const uri = 'mongodb+srv://test:0kUkd360hKNaraDd@location-testing.mny1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    const client = new MongoClient(uri)
+
+    try{
+        await client.connect();
+        console.log("Mongo DB connected");
+    }catch(e){
+        console.log("Mongo error : " + e);
+    }finally{
+        console.log("MongoDB connection closing");
+        await client.close()
     }
 }
